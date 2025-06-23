d86949199120dcf9f084ab433c112a98cb4532f5
NuhDev <92098247+NuhDevX@users.noreply.github.com>
2025-06-23 23:32:14 +0700

    Change language to Indonesian

--- src/DailyQuestForm.php
+++ src/DailyQuestForm.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace phuongaz\dailyquest;
 
-
 use jojoe77777\FormAPI\CustomForm;
 use phuongaz\dailyquest\util\Utils;
 use pocketmine\player\Player;
@@ -14,19 +13,19 @@ class DailyQuestForm extends CustomForm {
     public function __construct(Player $player, ?callable $callable = null) {
         parent::__construct($callable);
         $this->setTitle("§l§6Ｍｉｓｓｉｏｎ");
-        $this->addLabel("§f§lChỉ tiêu hôm nay là:");
+        $this->addLabel("§f§lTarget hari ini adalah:");
         $target = Utils::parseTarget(DailyQuest::getInstance()->getYamlProvider()->getTarget());
         $value = DailyQuest::getInstance()->getYamlProvider()->getTargetValue();
         $reward = DailyQuest::getInstance()->getYamlProvider()->getReward();
-        $this->addLabel("§f§l" . $target . " §ex" . $value . "§f bất kì");
-        $this->addLabel("§f§lPhần thưởng:§e ". $reward . " §fxu");
+        $this->addLabel("§f§l" . $target . " §ex" . $value . "§fkali");
+        $this->addLabel("§f§lHadiah:§e ". $reward . " §fxu");// xu apaan dah? ga bisa di translate jir
 
         $session = DailyQuest::getInstance()->getSessionManager()->getSession($player->getName());
         $completed = $session->getCompleted();
         if($completed >= $value) {
-            $this->addLabel("§a§lBạn đã hoàn thành chỉ tiêu hôm nay §e§l{$completed}§r§a/§e§l{$value}!");
+            $this->addLabel("§a§lAnda telah menyelesaikan quest §e§l{$completed}§r§a/§e§l{$value} hari ini!");
         } else {
-            $this->addLabel("§c§lBạn chưa hoàn thành chỉ tiêu hôm nay §e§l{$completed}§r§c/§e§l{$value}!");
+            $this->addLabel("§c§lAnda belum menyelesaikan quest Anda §e§l{$completed}§r§c/§e§l{$value} hari ini!");
         }
     }
 
